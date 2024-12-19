8e4d937a981acb75eef2c21a7d738cc0dc54da4a
Kevnkkm <mail@kevnkkm.de>
2024-12-19 03:13:50 +0100

    initial html and css (vanilla)
    jank, atm more a design preview, not responsive (the current one isn't aswell so...)

--- pages/[name].vue
+++ pages/[name].vue
@@ -1,6 +1,6 @@
 <template>
-	<div>
-		<div v-if="player" class="md:p-10 space-y-2">
+	<!-- <div>
+		<div v-if="player" class="md:p-10 space-y-2 max-w-[1800px]">
 			<div class="grid sm:grid-cols-1 lg:grid-cols-3 gap-4 py-8">
 				<div>
 					<a class="text-5xl py-8" :href="`https://discord.com/users/${player.discord}`">{{ player.name }}</a>
@@ -26,6 +26,52 @@
 			<div>
 				<p class="text-4xl">Extended History:</p>
 				<highchart :options="chartOptions" />
+			</div>
+		</div>
+		<div v-else class="text-5xl flex flex-col items-center py-10">
+			<p>This player does not exist</p>
+			<img src="/images/MK8D-PoliceRed.png" alt="error icon" width="200" height="200" />
+		</div>
+	</div> -->
+	<div>
+		<div v-if="player" class="content">
+			<div class="profile-container">
+				<div class="rank-icon-background">
+					<img
+						:src="`https://raw.githubusercontent.com/mk8dx-yuzu/ranks/refs/heads/main/${getRank(player.mmr, playerData.indexOf(player))}.png`"
+						alt="rank icon" />
+					<!-- <div class="gradient-blur">
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+						<div></div>
+					</div> -->
+					<div class="overlay">
+					<a class="player-name" :href="`https://discord.com/users/${player.discord}`">{{ player.name }}</a>
+					<div class="rank-info">
+						<p :class="[getColor(player.mmr, playerData.indexOf(player))]">{{ player.mmr }} MMR</p>
+						<p :class="[getColor(player.mmr, playerData.indexOf(player))]">●</p>
+						<p :class="[getColor(player.mmr, playerData.indexOf(player))]">{{ getRank(player.mmr, playerData.indexOf(player)) }} Rank</p>
+					</div>
+					<p>Rank #{{ playerData.findIndex((obj) => obj.name === player.name) + 1 }} serverwide</p>
+				</div>
+				</div>
+				<div class="stats">
+					<div class="stat">
+						<p class="stat-title">Your last 5 MMR changes:</p>
+						<p class="stat-value">{{ player.history.slice(-5).join(", ") }}</p>
+					</div>
+					<div class="stat">
+						<p class="stat-value"><b>{{ Math.round((player.wins / (player.wins + player.losses)) * 100) }}%</b> Winrate</p>
+						<p class="stat-title">out of <b>{{ player.wins + player.losses }}</b> total mogis</p>
+					</div>
+				</div>
+				<div class="history-container">
+					<!-- <p>Extended History:</p> -->
+					<highchart :options="chartOptions" />
+				</div>
 			</div>
 		</div>
 		<div v-else class="text-5xl flex flex-col items-center py-10">
