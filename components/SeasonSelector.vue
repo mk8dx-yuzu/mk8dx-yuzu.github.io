51265eba87b0e7e0eb0bd780575e179e246f95b1
probablyjassin <aouanijassin@gmail.com>
2025-07-23 21:07:04 +0200

    add s=3 query param on season toggle to s3

--- components/SeasonSelector.vue
+++ components/SeasonSelector.vue
@@ -2,19 +2,14 @@
 	<div class="season-selector">
 		<fieldset class="season-tabs" role="radiogroup" aria-labelledby="season-selector-label">
 			<legend id="season-selector-label" class="sr-only">Select Season</legend>
-			<label 
-				v-for="season in availableSeasons" 
-				:key="season.value" 
-				class="season-tab"
-			>
-				<input 
-					type="radio" 
-					:value="season.value" 
+			<label v-for="season in availableSeasons" :key="season.value" class="season-tab">
+				<input
+					type="radio"
+					:value="season.value"
 					v-model="selectedSeasonModel"
 					@change="handleSeasonChange"
 					name="season"
-					:aria-label="`Season ${season.value}`"
-				/>
+					:aria-label="`Season ${season.value}`" />
 				<span>{{ season.label }}</span>
 			</label>
 		</fieldset>
@@ -22,30 +17,41 @@
 </template>
 
 <script setup>
+	const route = useRoute();
+	const router = useRouter();
+
 	const props = defineProps({
 		modelValue: {
 			type: Number,
-			default: 4
+			default: 4,
 		},
 		seasons: {
 			type: Array,
 			default: () => [
-				{ value: 4, label: 'Season 4' },
-				{ value: 3, label: 'Season 3' }
-			]
-		}
-	})
+				{ value: 4, label: "Season 4" },
+				{ value: 3, label: "Season 3" },
+			],
+		},
+	});
 
-	const emit = defineEmits(['update:modelValue', 'change'])
+	const emit = defineEmits(["update:modelValue", "change"]);
 
-	const availableSeasons = computed(() => props.seasons)
+	const availableSeasons = computed(() => props.seasons);
 
 	const selectedSeasonModel = computed({
 		get: () => props.modelValue,
-		set: (value) => emit('update:modelValue', value)
-	})
+		set: (value) => emit("update:modelValue", value),
+	});
 
 	function handleSeasonChange() {
-		emit('change', selectedSeasonModel.value)
+		emit("change", selectedSeasonModel.value);
+		console.log(selectedSeasonModel.value);
+		if (selectedSeasonModel.value == 3) {
+			router.replace({ path: route.path, query: { ...route.query, s: 3 } });
+		} else if (selectedSeasonModel.value == 4) {
+			const newQuery = { ...route.query };
+			delete newQuery["s"];
+			router.replace({ path: route.path, query: newQuery });
+		}
 	}
 </script>
