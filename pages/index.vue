86c6da041c7a7c141ba9ba9c3391811d05a63fd2
Kevnkkm <mail@kevnkkm.de>
2025-07-28 21:16:00 +0200

    update link on season 4 notice banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<NoticeBanner color="purple">
-			<p>Season 4 released! Check out the changes <a href="https://github.com/probablyjassin/bot-mk8dx?tab=readme-ov-file#season-3-of-mk8dx-lounge-on-yuzu-online" target="_blank">here!</a></p>
+			<p>Season 4 released! Check out the changes <a href="https://github.com/probablyjassin/bot-mk8dx-public/blob/main/README.md#the-4th-season-of-mk8dx-lounge-on-yuzu-online" target="_blank">here!</a></p>
 		</NoticeBanner>
 		<div class="title">
 			<h1 class="text-center">Leaderboard</h1>
