9f44d719532fd3e69169ee573350d80c83b590cf
Kevnkkm <mail@kevnkkm.de>
2026-02-01 19:12:24 +0100

    correctly format/refactor all components

--- app/components/ErrorTxt.vue
+++ app/components/ErrorTxt.vue
@@ -1,9 +1,14 @@
 <template>
-	<div id="api-error" class="api-error flex">
-		<img src="/images/MK8D-PoliceRed.png" />
-		<div class="error-text">
-			<h1 class="font-bold text-2xl">Error!</h1>
-			<p>There was a problem getting the data.<br />Please try again later.</p>
-		</div>
-	</div>
+  <div
+    id="api-error"
+    class="api-error flex"
+  >
+    <img src="/images/MK8D-PoliceRed.png">
+    <div class="error-text">
+      <h1 class="font-bold text-2xl">
+        Error!
+      </h1>
+      <p>There was a problem getting the data.<br>Please try again later.</p>
+    </div>
+  </div>
 </template>
